d495d76bae783c8a951575a2888533d80f15a101
Ashish K <141225901+ash7724@users.noreply.github.com>
2025-04-16 19:29:11 +0530

    Create Mod_N_counter.v

---
+++ day7/Mod_N_counter.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module MOD_N_Counter #(parameter N=3) (
+  input  clk,
+  input  resetn,
+  output pulse_out
+    );
+    
+
+    reg [$clog2(N)-1:0] count_val;
+    always @(posedge clk) begin
+      if(!resetn)
+          count_val<='h0;
+      else if(count_val==N)
+          count_val<='h0;
+      else
+          count_val<=count_val+1'b1;
+    end
+    
+    assign pulse_out=(count_val==N);
+endmodule
