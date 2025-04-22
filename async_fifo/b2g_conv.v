3133bcf5af2ecd59bfa0e07e00500b5df275368d
Ashish K <141225901+ash7724@users.noreply.github.com>
2025-04-23 00:23:53 +0530

    Update b2g_conv.v

--- async_fifo/b2g_conv.v
+++ async_fifo/b2g_conv.v
@@ -1,12 +1,12 @@
 module b2g_conv #(parameter DEPTH=8) ( 
-  input   [DEPTH-1 : 0] bin_num, 
-  output  [DEPTH-1 : 0] grey_num 
+  input   [DEPTH : 0] bin_num, 
+  output  [DEPTH : 0] grey_num 
 );
    genvar i;
    generate
-     for (i=0; i<DEPTH-1;i=i+1) begin
+     for (i=0; i<DEPTH;i=i+1) begin
       assign grey_num[i] = bin_num[i] ^ bin_num[i+1];
      end
    endgenerate;
-   assign grey_num[DEPTH-1]= bin_num[DEPTH-1];
+   assign grey_num[DEPTH]= bin_num[DEPTH];
 endmodule;
