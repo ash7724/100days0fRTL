50eea8ea8c3e26cdc27ca81dff79175a6a9d9a31
Ashish K <141225901+ash7724@users.noreply.github.com>
2025-04-23 00:24:12 +0530

    Update g2b_conv.v

--- async_fifo/g2b_conv.v
+++ async_fifo/g2b_conv.v
@@ -1,10 +1,11 @@
+
 module g2b_conv #(parameter DEPTH=8) ( 
-  input   [DEPTH-1 : 0] grey_num, 
-  output  [DEPTH-1 : 0] bin_num 
+  input   [DEPTH : 0] grey_num, 
+  output  [DEPTH : 0] bin_num 
 );
    genvar i;
    generate
-     for (i=0; i<DEPTH;i=i+1) begin
+     for (i=0; i<=DEPTH;i=i+1) begin
       assign bin_num[i] = ^(grey_num >> i);
      end
    endgenerate;
